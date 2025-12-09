034a3eef48a194bc60f504bea0f8187c7641f55b
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2025-12-09 21:08:02 +0100

    Differentiate temporary cards, update card art

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A quick website to use as the monster attack deck when playing gloomhaven
+A quick website to use as the monster attack deck when playing Gloomhaven
+
+Card art by Rasmus Lönnkvist
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Deck from "./deck.js";
-let deck = new Deck(JSON.parse(localStorage.getItem('deck')));
+let deck = new Deck(JSON.parse(localStorage.getItem("deck")));
 let currentCard = null;
 let previousCard = null;
 let mostRecentCardDiv;
@@ -11,79 +11,81 @@ let backsideImg;
 let reminder;
 let blessCounter;
 let curseCounter;
-let emptyCardImage = 'GH_Trans.png';
-let backsideImage = 'GH_Backside.png';
+let emptyCardImage = "GH_Trans.png";
+let backsideImage = "GH_Backside.png";
 
 addEventListener("DOMContentLoaded", (event) => {
-    mostRecentCardDiv = document.getElementById('mostRecentCard');
-    mostRecentCardImg = document.getElementById('mostRecentCardImg');
-    secondMostRecentCardDiv = document.getElementById('secondMostRecentCard');
-    secondMostRecentCardImg = document.getElementById('secondMostRecentCardImg');
-    backsideDiv = document.getElementById('backsideDiv');
-    backsideImg = document.getElementById('backsideImg');
-    reminder = document.getElementById('reminderIcon');
-    blessCounter = document.getElementById('blessCounter');
-    curseCounter = document.getElementById('curseCounter');
-    [backsideDiv, mostRecentCardDiv, secondMostRecentCardDiv].forEach(div => div.addEventListener("click", e => {
-        drawNewCard();
-    }));
-    document.getElementById('reshuffleButton').addEventListener("click", e => {
-        reshuffle();
-        updateTexts();
-    });
-    document.getElementById('blessButton').addEventListener("click", e => {
-        deck.addCard('x2', 'GH_x2.png');updateTexts();
-        saveDeck();
-    });
-    document.getElementById('curseButton').addEventListener("click", e => {
-        deck.addCard('∅', 'GH_Null.png');updateTexts();
-        saveDeck();
-    });
-    currentCard = deck.discardPile[deck.discardPile.length - 1];
-    previousCard = deck.discardPile[deck.discardPile.length - 2];
+  mostRecentCardDiv = document.getElementById("mostRecentCard");
+  mostRecentCardImg = document.getElementById("mostRecentCardImg");
+  secondMostRecentCardDiv = document.getElementById("secondMostRecentCard");
+  secondMostRecentCardImg = document.getElementById("secondMostRecentCardImg");
+  backsideDiv = document.getElementById("backsideDiv");
+  backsideImg = document.getElementById("backsideImg");
+  reminder = document.getElementById("reminderIcon");
+  blessCounter = document.getElementById("blessCounter");
+  curseCounter = document.getElementById("curseCounter");
+  [backsideDiv, mostRecentCardDiv, secondMostRecentCardDiv].forEach((div) =>
+    div.addEventListener("click", (e) => {
+      drawNewCard();
+    })
+  );
+  document.getElementById("reshuffleButton").addEventListener("click", (e) => {
+    reshuffle();
     updateTexts();
+  });
+  document.getElementById("blessButton").addEventListener("click", (e) => {
+    deck.addCard("x2", "GH_Bless.png");
+    updateTexts();
+    saveDeck();
+  });
+  document.getElementById("curseButton").addEventListener("click", (e) => {
+    deck.addCard("∅", "GH_Curse.png");
+    updateTexts();
+    saveDeck();
+  });
+  currentCard = deck.discardPile[deck.discardPile.length - 1];
+  previousCard = deck.discardPile[deck.discardPile.length - 2];
+  updateTexts();
 });
 
 function drawNewCard() {
-    if (currentCard){
-        previousCard = currentCard;
-    }
-    currentCard = deck.drawCard();
-    updateTexts();
-    saveDeck();
+  if (currentCard) {
+    previousCard = currentCard;
+  }
+  currentCard = deck.drawCard();
+  updateTexts();
+  saveDeck();
 }
 
-function updateTexts(){
-    secondMostRecentCardImg.src = previousCard?.imgSrc ?? emptyCardImage;
-    if (currentCard === null) {
-        mostRecentCardImg.innerText = 'Deck empty';
-        mostRecentCardImg.src = emptyCardImage;
-    }
-    else{
-        mostRecentCardImg.
-        src = currentCard?.imgSrc ?? emptyCardImage;
-    }
-    backsideImg.src = deck.deck.length > 0 ? backsideImage : emptyCardImage;
-    blessCounter.innerText = deck.countBlesses();
-    curseCounter.innerText = deck.countCurses();
-
-    if (deck.shouldReshuffle) {
-        reminder.classList.remove('inactive');
-    }
-}
-
-function reshuffle(){
-    deck.reshuffleDeck();
-    secondMostRecentCardImg.src = emptyCardImage;
+function updateTexts() {
+  secondMostRecentCardImg.src = previousCard?.imgSrc ?? emptyCardImage;
+  if (currentCard === null) {
+    mostRecentCardImg.innerText = "Deck empty";
     mostRecentCardImg.src = emptyCardImage;
-    reminder.classList.add('inactive');
-    blessCounter.innerText = deck.countBlesses();
-    curseCounter.innerText = deck.countCurses();
-    currentCard = null;
-    previousCard = null;
-    saveDeck();
+  } else {
+    mostRecentCardImg.src = currentCard?.imgSrc ?? emptyCardImage;
+  }
+  backsideImg.src = deck.deck.length > 0 ? backsideImage : emptyCardImage;
+  blessCounter.innerText = deck.countBlesses();
+  curseCounter.innerText = deck.countCurses();
+
+  if (deck.shouldReshuffle) {
+    reminder.classList.remove("inactive");
+  }
+}
+
+function reshuffle() {
+  deck.reshuffleDeck();
+  secondMostRecentCardImg.src = emptyCardImage;
+  mostRecentCardImg.src = emptyCardImage;
+  reminder.classList.add("inactive");
+  blessCounter.innerText = deck.countBlesses();
+  curseCounter.innerText = deck.countCurses();
+  currentCard = null;
+  previousCard = null;
+  saveDeck();
 }
 
 function saveDeck() {
-    localStorage.setItem('deck', JSON.stringify(deck));
+  localStorage.setItem("deck", JSON.stringify(deck));
 }
